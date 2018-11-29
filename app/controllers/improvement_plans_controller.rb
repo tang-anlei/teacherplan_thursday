0dcb0a4a5120e1c355d0c33acd5735416aad7691
Raghu Betina <raghu@firstdraft.com>
2018-11-29 21:00:33 +0000

    Only improvement_plan user can modify improvement_plan

--- app/controllers/improvement_plans_controller.rb
+++ app/controllers/improvement_plans_controller.rb
@@ -1,4 +1,14 @@
 class ImprovementPlansController < ApplicationController
+  before_action :current_user_must_be_improvement_plan_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_improvement_plan_user
+    improvement_plan = ImprovementPlan.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == improvement_plan.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @improvement_plans = ImprovementPlan.all
 
