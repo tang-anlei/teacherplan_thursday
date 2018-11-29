3f2081c8b3800582b4399ab4bde41c47a3047b84
Raghu Betina <raghu@firstdraft.com>
2018-11-29 20:59:31 +0000

    Direct association between improvement_plans and goals

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Goal, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:improvement_plan) }
+
     end
 
     describe "InDirect Associations" do
--- spec/models/improvement_plan_spec.rb
+++ spec/models/improvement_plan_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe ImprovementPlan, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:goals) }
+
     it { should belong_to(:user) }
 
     end
