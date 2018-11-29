32ec232b1c90960c388951fb162ed253971ecb69
Raghu Betina <raghu@firstdraft.com>
2018-11-29 20:59:18 +0000

    Generate goals resource with draft:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Goal resource:
+
+  # CREATE
+  get("/goals/new", { :controller => "goals", :action => "new_form" })
+  post("/create_goal", { :controller => "goals", :action => "create_row" })
+
+  # READ
+  get("/goals", { :controller => "goals", :action => "index" })
+  get("/goals/:id_to_display", { :controller => "goals", :action => "show" })
+
+  # UPDATE
+  get("/goals/:prefill_with_id/edit", { :controller => "goals", :action => "edit_form" })
+  post("/update_goal/:id_to_modify", { :controller => "goals", :action => "update_row" })
+
+  # DELETE
+  get("/delete_goal/:id_to_remove", { :controller => "goals", :action => "destroy_row" })
+
+  #------------------------------
+
   # Routes for the Improvement plan resource:
 
   # CREATE
